8cafac64da048cdd53db1d51c7f63d0f66eb95e2
marcelabus <guidoymarcela@hotmail.com>
2022-06-28 12:07:07 -0300

    microdesafío clase 15

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const publicPath = path.resolve (__dirname, './public');
 
 app.use ( express.static (publicPath));
 
-app.get ('/Home', (req,res) => {
+app.get ('/home', (req,res) => {
     res.sendFile ( path.resolve (__dirname, './views/index.html'));
 });
 
-app.get ('/Crear-producto', (req,res) => {
-    res.sendFile ( path.resolve (__dirname, './views/crearProducto.html'));
+app.get ('/register', (req,res) => {
+    res.sendFile ( path.resolve (__dirname, './views/register.html'));
+});
+
+app.get ('/login', (req,res) => {
+    res.sendFile ( path.resolve (__dirname, './views/login.html'));
 });
 
 app.listen (3000, () => {
