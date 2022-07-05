5892c868f678b6c7cbc8b1b3001393442e5f7de8
marcelabus <guidoymarcela@hotmail.com>
2022-07-04 21:08:25 -0300

    modifiqué puerto en app.js

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.get ('/login', (req,res) => {
     res.sendFile ( path.resolve (__dirname, './views/login.html'));
 });
 
-app.listen (3000, () => {
+app.listen (process.env.PORT || 3000, () => {
     console.log ('Servidor levantado en puerto 3000');
 });
